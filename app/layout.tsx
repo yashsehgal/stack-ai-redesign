c5702edd0be5c98905a0861a1ff0c99ad610ec6c
Yash Sehgal <yashsehgal.work@gmail.com>
2024-01-26 18:21:25 +0530

    🎨 Improve footer and CTA section in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/sections';
-import FooterSection from '@/components/sections/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,7 +21,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
-        <FooterSection />
       </body>
     </html>
   );
