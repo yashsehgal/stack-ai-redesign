5f2d70c24cc3e16c841be139b8d8d22f7d91827a
Yash Sehgal <yashsehgal.work@gmail.com>
2024-01-27 22:17:32 +0530

    ✨ Add favicon to page head

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
       </body>
-    </html>
+    </html >
   );
 }
